9c6248b1f0e2d12d68f392a6e1f730c1bd273170
BrunodCreator <bruno.dqueirozmt@gmail.com>
2024-11-16 17:42:41 -0300

    iteração funcionando para cada um dos municípios, ainda sem salvar o saldo

--- capturar_tipo_de_conta_e_saldo.py
+++ capturar_tipo_de_conta_e_saldo.py
@@ -12,23 +12,29 @@ service = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=service)
 
 # Acessa a página
-navegador.get('https://aplicacoes.mds.gov.br/suaswebcons/restrito/execute.jsf?b=*tbmepQbsdfmbtQbhbtNC&event=*fyjcjs')
+url = ('https://aplicacoes.mds.gov.br/suaswebcons/restrito/execute.jsf?b=*tbmepQbsdfmbtQbhbtNC&event=*fyjcjs')
+navegador.get(url)
 
 # Preenche os campos que ativam o dropdown de município
+
 ano = Select(navegador.find_element(By.XPATH, '//*[@id="form:ano"]'))
 mes = Select(navegador.find_element(By.XPATH, '//*[@id="form:mes"]'))
 esf_adm = Select(navegador.find_element(By.XPATH, '//*[@id="form:esferaAdministrativa"]'))
 uf = Select(navegador.find_element(By.XPATH, '//*[@id="form:uf"]'))
 
+
 # Define os valores necessários para ativar o campo de município
-mes.select_by_value('10')
-sleep(1)
-uf.select_by_visible_text('GO')
-sleep(1)
-ano.select_by_visible_text('2024')
-sleep(1)
-esf_adm.select_by_visible_text('MUNICIPAL')
-sleep(1)
+def selecionar_valores_elementos():
+    mes.select_by_value('10')
+    sleep(1)
+    uf.select_by_visible_text('GO')
+    sleep(1)
+    ano.select_by_visible_text('2024')
+    sleep(1)
+    esf_adm.select_by_visible_text('MUNICIPAL')
+    sleep(1)
+
+selecionar_valores_elementos()
 
 # Aguarda o campo 'municipio' ser ativado (não estar mais 'disabled')
 municipio_dropdown = WebDriverWait(navegador, 10).until(
@@ -36,10 +42,31 @@ municipio_dropdown = WebDriverWait(navegador, 10).until(
 )
 municipio = Select(municipio_dropdown)
 
-# Seleciona o município desejado
-municipio.select_by_visible_text('ABADIANIA')
-sleep(1)
+municipios_valores = [option.get_attribute("value") for option in municipio.options if option.get_attribute("value")]
 
-botao_pesquisar = navegador.find_element(By.ID, "form:pesquisar")
-botao_pesquisar.click()
-sleep(15)
+# municipio.select_by_visible_text('ABADIANIA')
+# sleep(1)
+for valor in municipios_valores:
+    sleep(5)
+    
+    ano = Select(navegador.find_element(By.XPATH, '//*[@id="form:ano"]'))
+    mes = Select(navegador.find_element(By.XPATH, '//*[@id="form:mes"]'))
+    esf_adm = Select(navegador.find_element(By.XPATH, '//*[@id="form:esferaAdministrativa"]'))
+    uf = Select(navegador.find_element(By.XPATH, '//*[@id="form:uf"]'))
+    selecionar_valores_elementos()
+
+    # Aguarda o campo 'municipio' ser ativado novamente
+    municipio_dropdown = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "#form\\:municipio")))
+    municipio = Select(municipio_dropdown)
+
+    municipio.select_by_value(valor)
+    sleep(1)
+
+    # Clica no botão de pesquisa
+    botao_pesquisar = navegador.find_element(By.ID, "form:pesquisar")
+    botao_pesquisar.click()
+    sleep(10)
+    
+    
+navegador.quit()
