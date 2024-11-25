b40d37b6fc48d396279359a2cad68c39f9ea7059
BrunodCreator <bruno.dqueirozmt@gmail.com>
2024-11-25 17:04:48 -0300

    Versão mais estável, enviada para o joão

--- capturar_tipo_de_conta_e_saldo.py
+++ capturar_tipo_de_conta_e_saldo.py
@@ -12,7 +12,7 @@ import os
 primeira_execucao = False
 
 nome_da_planilha = str(input('Digite o nome da planilha (sem extensão): ')) 
-ano_elemento_selecionado = str(input('Digite o ano com 4 digitos'))
+ano_elemento_selecionado = str(input('Digite o ano com 4 digitos: '))
 mes_elemento_selecionado = str(input('''DIGITE O NÚMERO DO MÊS COM 2 DÍGITOS:
 [01] JANEIRO
 [02] FEVEREIRO
@@ -51,16 +51,16 @@ navegador.get(url)
 
 # Torna os campos que ativam o dropdown de município selecionáveis
 def selecionar_elementos(navegador):
-    ano = Select(WebDriverWait(navegador, 10).until(
+    ano = Select(WebDriverWait(navegador, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="form:ano"]'))
     ))
-    mes = Select(WebDriverWait(navegador, 10).until(
+    mes = Select(WebDriverWait(navegador, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="form:mes"]'))
     ))
-    esf_adm = Select(WebDriverWait(navegador, 10).until(
+    esf_adm = Select(WebDriverWait(navegador, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="form:esferaAdministrativa"]'))
     ))
-    uf = Select(WebDriverWait(navegador, 10).until(
+    uf = Select(WebDriverWait(navegador, 20).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="form:uf"]'))
     ))
 
@@ -84,7 +84,7 @@ def click_pesquisar():
         botao_pesquisar.click()
 
         # Aguarda até que o elemento "valor_elemento" esteja disponível (ou o tempo limite expire)
-        WebDriverWait(navegador, 20).until(
+        WebDriverWait(navegador, 30).until(
             EC.presence_of_element_located((By.XPATH, '//td[@class="tdParNum"]/table/tbody/tr/td[2]/span'))
         )
         print("Página carregada e elemento encontrado.")
@@ -93,6 +93,7 @@ def click_pesquisar():
 
 while primeira_execucao is True:
     
+    navegador.get(url)
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     sheet.title = "Saldo dos Municípios"
@@ -108,14 +109,17 @@ while primeira_execucao is True:
     selecionar_valores_elementos()
     
     # Aguarda o campo 'municipio' ser ativado (não estar mais 'disabled')
-    municipio_dropdown = WebDriverWait(navegador, 10).until(
+    municipio_dropdown = WebDriverWait(navegador, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "#form\\:municipio"))
         )
     municipio = Select(municipio_dropdown)
+    
 
     municipios_valores = [option.get_attribute("value") for option in municipio.options if option.get_attribute("value")]
 
     for valor in municipios_valores:
+        if valor == '522230':
+            primeira_execucao = False
         try:
             navegador.refresh()
             ano, mes, esf_adm, uf = selecionar_elementos(navegador)
@@ -123,11 +127,12 @@ while primeira_execucao is True:
             selecionar_valores_elementos()
 
             # Aguarda o campo 'municipio' ser ativado novamente
-            municipio_dropdown = WebDriverWait(navegador, 10).until(
+            municipio_dropdown = WebDriverWait(navegador, 20).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, "#form\\:municipio")))
             municipio = Select(municipio_dropdown)
             municipio.select_by_value(valor)
             selecao_nome_municipio = municipio.first_selected_option.text
+            
 
             click_pesquisar()
 
@@ -151,7 +156,7 @@ while primeira_execucao is True:
                 sheet[f'C{linha_excel}'] = 'NAO CONCLUIDO'
                 print(f'Deu na captura do valor da linha: {selecao_nome_municipio}')
                 linha_excel += 1
-                navegador.refresh()
+                
             
             workbook.save(caminho_area_de_trabalho)
             print(f'Dados salvos em {caminho_area_de_trabalho}.')
@@ -161,60 +166,59 @@ while primeira_execucao is True:
             sheet[f'C{linha_excel}'] = 'NAO CONCLUIDO'
             print(f'Deu erro na seleção dos campos na linha {selecao_nome_municipio}')
             linha_excel += 1
-            navegador.refresh()
+    primeira_execucao = False       
 
-primeira_execucao = False
+if primeira_execucao is False:
 
+    workbook = openpyxl.load_workbook(caminho_area_de_trabalho)
+    sheet = workbook.active 
 
-workbook = openpyxl.load_workbook(caminho_area_de_trabalho)
-sheet = workbook.active 
+    # Itera sobre as linhas da coluna "SALDO" (B), começando pela segunda linha
+    # Itera sobre as linhas da coluna "SALDO" (coluna B)
 
-# Itera sobre as linhas da coluna "SALDO" (B), começando pela segunda linha
-# Itera sobre as linhas da coluna "SALDO" (coluna B)
-
-for row in sheet.iter_rows(min_row=2, max_col=2, values_only=False):  # Apenas a coluna B
-    municipio_celula = row[0]
-    saldo_celula = row[1]  # Acessa a célula na coluna B (apenas uma célula por linha)
-    
-    
-    if saldo_celula.value is None:
-        nome_municipio = municipio_celula.value
-        # saldo_celula.value = 'outro'
-
-        try:
-            navegador.get(url)
-            sleep(3)
-            
-            ano, mes, esf_adm, uf = selecionar_elementos(navegador)
-            
-            # Define os valores necessários para ativar o campo de município
-            selecionar_valores_elementos()
-
-            # Aguarda o campo 'municipio' ser ativado novamente
-            municipio_dropdown = WebDriverWait(navegador, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, "#form\\:municipio")))
-            municipio = Select(municipio_dropdown)
-            municipio.select_by_visible_text(nome_municipio)
-
-            click_pesquisar()
+    for row in sheet.iter_rows(min_row=2, max_col=2, values_only=False):  # Apenas a coluna B
+        municipio_celula = row[0]
+        saldo_celula = row[1]  # Acessa a célula na coluna B (apenas uma célula por linha)
+        
+        
+        if saldo_celula.value is None:
+            nome_municipio = municipio_celula.value
+            # saldo_celula.value = 'outro'
 
             try:
-                # Localiza o segundo <td> dentro da estrutura <td> -> <table> -> <tbody> -> <tr> -> <td>
-                valor_elemento = navegador.find_element(By.XPATH, 
-                    '//td[@class="tdParNum"]/table/tbody/tr/td[2]/span')
+                navegador.get(url)
+                sleep(3)
                 
-                # Extrai o texto visível do elemento
-                valor_saldo = valor_elemento.text.strip()
-                saldo_celula.value = valor_saldo
-                print(f"corrigido: {nome_municipio} com o valor: {valor_saldo}")
-                workbook.save(caminho_area_de_trabalho)
+                ano, mes, esf_adm, uf = selecionar_elementos(navegador)
                 
-            except Exception as e:
-                print(f'Deu erro na captura do valor da linha: {nome_municipio}')
-                navegador.refresh()
+                # Define os valores necessários para ativar o campo de município
+                selecionar_valores_elementos()
 
-        except Exception as t:
-            print(f'Deu erro na seleção dos campos na linha: {nome_municipio}|info do erro: {t}')
+                # Aguarda o campo 'municipio' ser ativado novamente
+                municipio_dropdown = WebDriverWait(navegador, 20).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, "#form\\:municipio")))
+                municipio = Select(municipio_dropdown)
+                municipio.select_by_visible_text(nome_municipio)
+
+                click_pesquisar()
+
+                try:
+                    # Localiza o segundo <td> dentro da estrutura <td> -> <table> -> <tbody> -> <tr> -> <td>
+                    valor_elemento = navegador.find_element(By.XPATH, 
+                        '//td[@class="tdParNum"]/table/tbody/tr/td[2]/span')
+                    
+                    # Extrai o texto visível do elemento
+                    valor_saldo = valor_elemento.text.strip()
+                    saldo_celula.value = valor_saldo
+                    print(f"corrigido: {nome_municipio} com o valor: {valor_saldo}")
+                    workbook.save(caminho_area_de_trabalho)
+                    
+                except Exception as e:
+                    print(f'Deu erro na captura do valor da linha: {nome_municipio}')
+                    navegador.refresh()
+
+            except Exception as t:
+                print(f'Deu erro na seleção dos campos na linha: {nome_municipio}|info do erro: {t}')
             
 print('ENCERRANDO O PROGRAMA...')
 navegador.quit()
